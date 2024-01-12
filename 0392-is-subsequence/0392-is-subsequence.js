1e1b67c9be5af0245ef39f10ccc33c19a8ae622c
Dororo <66819791+jcdororo@users.noreply.github.com>
2024-01-12 21:09:23 +0900

    Time: 63 ms (16.63%), Space: 43 MB (7.59%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isSubsequence = function(s, t) {
+   
+    let index = 0;
+
+    for(let i = 0; i < t.length; i++) {
+        if(s[index] == t[i]) {
+            index++
+        }
+    }
+
+    if(index == s.length) {
+        return true;
+    } else {
+        return false;
+    }
+
+
+
+    
+};
