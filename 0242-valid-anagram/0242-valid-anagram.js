caff8dc857217e956bb2f5b80218b97b57f38156
Dororo <66819791+jcdororo@users.noreply.github.com>
2024-01-24 20:20:23 +0900

    Time: 83 ms (34.90%), Space: 53.6 MB (5.03%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -0,0 +1,14 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+
+    const strS = s.split('').sort().join('')
+    const strT = t.split('').sort().join('')
+
+    return strS == strT
+
+    
+};
